6bcd37c2a7e926d77032cd01d6840b0289d750cb
longhoang94 <longsutls@gmail.com>
2019-01-15 16:45:45 +0700

    add test-case an edge a less than zero

---
+++ src/test/java/TestingTriangleTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestingTriangleTest {
+    @Test
+    public void edgeALessThanZero() {
+        double a = -1;
+        double b = 2;
+        double c = 3;
+
+        String actual = TestingTriangle.isTriangle(a, b, c);
+        String expected = "Not a triangle";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void edgeBLessThanZero() {
+        double a = 1;
+        double b = -1;
+        double c = 3;
+
+        String actual = TestingTriangle.isTriangle(a, b, c);
+        String expected = "Not a triangle";
+
+        Assert.assertEquals(actual, expected);
+    }
+}
