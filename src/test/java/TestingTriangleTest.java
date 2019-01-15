57b93b9874f6fa52655dd45c312c7092c1e0d7fc
longhoang94 <longsutls@gmail.com>
2019-01-15 17:25:12 +0700

    pass test-case isAIsoscelesTriangle

--- src/test/java/TestingTriangleTest.java
+++ src/test/java/TestingTriangleTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class TestingTriangleTest {
     @Test
     public void edgeALessThanZero() {
-        double a = -1;
+        double a = -1.9;
         double b = 2;
         double c = 3;
 
@@ -20,7 +20,7 @@ public class TestingTriangleTest {
     public void edgeBLessThanZero() {
         double a = 1;
         double b = -1;
-        double c = 3;
+        double c = 3.1;
 
         String actual = TestingTriangle.isTriangle(a, b, c);
         String expected = "Not a triangle";
@@ -31,7 +31,7 @@ public class TestingTriangleTest {
     @Test
     public void edgeCLessThanZero() {
         double a = 1;
-        double b = 2;
+        double b = 2.3;
         double c = -1;
 
         String actual = TestingTriangle.isTriangle(a, b, c);
@@ -72,6 +72,30 @@ public class TestingTriangleTest {
 
         String actual = TestingTriangle.isTriangle(a, b, c);
         String expected = "Not a triangle";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void sumTwoEdgesLessThanTheOtherV2() {
+        double a = 5.6;
+        double b = 2.3;
+        double c = 2.1;
+
+        String actual = TestingTriangle.isTriangle(a, b, c);
+        String expected = "Not a triangle";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void isAIsoscelesTriangle() {
+        double a = 3.4;
+        double b = 3.4;
+        double c = 6;
+
+        String actual = TestingTriangle.isTriangle(a, b, c);
+        String expected = "Is a isosceles triangle";
 
         Assert.assertEquals(actual, expected);
     }
